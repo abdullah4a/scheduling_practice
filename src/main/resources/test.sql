d10f93b478030c3522b134a3f3dd13fb60192b69
abdullah4a <abdullahkhan4a@gmail.com>
2022-01-13 17:38:30 +0500

    SQL Practice

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -22,3 +22,24 @@ Grant INSERT,DELETE ON test_table TO current_user;
 
 -- Alias
 SELECT id as UserID FROM test_table;
+-- //////////////////
+-- create table if not exists practice_table
+-- (
+--     web_Id      BIGINT  not null primary key unique,
+--     firstName   varchar not null,
+--     lastName    varchar,
+--     dob         varchar,
+--     gender      varchar,
+--     dateCreated date DEFAULT CURRENT_TIMESTAMP,
+--     address     varchar
+-- );
+INSERT INTO public.practice_table (web_id, firstname, lastname, dob, gender, datecreated, address)
+VALUES (44424, 'TEST', 'Model', '12-02-1265', 'male', DEFAULT, null);
+
+
+Select web_Id as webId, firstName,lastName,dateCreated From practice_table;
+
+update practice_table SET firstName='Numan',lastName='Razzaq',gender='Male' where web_id=1232131;
+Select * From practice_table;
+
+SELECT * FROM practice_table order by web_id Desc ;
